119080e697e5402540c004f6759c1df99e4536ea
amol <amolp@ec-mobility.biz>
2020-09-23 21:58:37 +0530

    Solution for assignment 02

--- assignment_02/src/App.js
+++ assignment_02/src/App.js
@@ -1,18 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import ValidationComponent from './ValidationComponent/ValidationComponent';
+import CharComponent from './CharComponent/CharComponent';
 
 class App extends Component {
+  state = {
+    userData: ''
+  }
+  inputChnagedHandler = (event) => {
+    this.setState({ userData: event.target.value });
+  }
+  deleteCHarHandler = (index) => {
+    const text = this.state.userData.split('')
+    text.splice(index, 1);
+    const updatedText = text.join('');
+    this.setState({ userData: updatedText });
+
+  }
   render() {
+    let charList = this.state.userData.split('').map((ch, index) => {
+      return (<CharComponent character={ch} key={index}
+        clicked={() => this.deleteCHarHandler(index)} />)
+    });
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input type="text" onChange={this.inputChnagedHandler} value={this.state.userData} />
+        <p>{this.state.userData}</p>
+        <ValidationComponent inputLength={this.state.userData.length}></ValidationComponent>
+        {charList}
       </div>
     );
   }
